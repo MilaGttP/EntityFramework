2e19f582e1683540598044f4c4f50e3af7143590
Mila <101627933+MilaGttP@users.noreply.github.com>
2022-12-02 21:08:09 +0200

    Add files via upload

--- ModelsLibrary/Models.cs
+++ ModelsLibrary/Models.cs
@@ -1,4 +1,4 @@
-
+﻿
 namespace Models
 {
     public class Team
@@ -9,5 +9,26 @@ namespace Models
         public int ? VictoriesAmount { get; set; }
         public int ? DefeatsAmount { get; set; }
         public int ? DrawsAmount { get; set; }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string ? FullName { get; set; }
+        public string ? Country { get; set; }
+        public int ? Number { get; set; }
+        public string ? Position { get; set; }
+        public int ? TeamID { get; set;}
+    }
+
+    public class Playings
+    { 
+        public int ID { get; set; }
+        public int ? FirstTeamID { get; set; }
+        public int ? SecondTeamID { get; set; }
+        public int ? FTeamGoals { get; set; }
+        public int ? STeamGoals { get; set; }
+        public int ? PersonID { get; set; }
+        public DateOnly ? PlayingDate { get; set; }
     }
 }
